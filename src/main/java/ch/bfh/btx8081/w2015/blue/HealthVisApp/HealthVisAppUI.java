c6c17d56bbcc8c54c1b446a23b828ab2aa405701
dornt1 <timlucas.dorner@students.bfh.ch>
2015-11-25 13:58:57 +0100

    add CalendarView

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/HealthVisAppUI.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/HealthVisAppUI.java
@@ -2,6 +2,8 @@ package ch.bfh.btx8081.w2015.blue.HealthVisApp;
 
 import javax.servlet.annotation.WebServlet;
 
+import ch.bfh.btx8081.w2015.blue.HealthVisApp.View.CalendarView;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
@@ -9,9 +11,14 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Calendar;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.calendar.event.BasicEventProvider;
+import com.vaadin.ui.components.calendar.event.CalendarEventProvider;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  *
@@ -20,21 +27,31 @@ import com.vaadin.ui.VerticalLayout;
 @Widgetset("ch.bfh.btx8081.w2015.blue.HealthVisApp.HealthVisAppWidgetset")
 public class HealthVisAppUI extends UI {
 
+	final String HEIGHT = "568";
+	final String WIDTH = "320";
+	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
+        layout.setHeight(HEIGHT);
+        layout.setWidth(WIDTH);
         layout.setMargin(true);
         setContent(layout);
-
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-
+     
+        TabSheet ts = new TabSheet();
+        ts.setHeight(568.0f, Unit.PERCENTAGE);
+        ts.setWidth(320.0f, Unit.PERCENTAGE);
+        ts.addStyleName(ValoTheme.TABSHEET_FRAMED);
+        ts.addStyleName(ValoTheme.TABSHEET_PADDED_TABBAR);
+        
+        Calendar cal = CalendarView.initCalendarView();
+        
+        final VerticalLayout l = new VerticalLayout(cal);
+        l.setWidth(568.0f, Unit.PERCENTAGE);
+        l.setHeight(320.0f, Unit.PERCENTAGE);
+        ts.addTab(l, "Calendar");
+        
+        layout.addComponent(ts);
     }
 
     @WebServlet(urlPatterns = "/*", name = "HealthVisAppUIServlet", asyncSupported = true)
--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/CalendarView.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/CalendarView.java
@@ -1,5 +1,28 @@
 package ch.bfh.btx8081.w2015.blue.HealthVisApp.View;
 
+import com.vaadin.ui.Calendar;
+import com.vaadin.ui.components.calendar.event.BasicEventProvider;
+import com.vaadin.ui.components.calendar.event.CalendarEventProvider;
+
 public class CalendarView {
 
+	
+	public static Calendar initCalendarView()
+	{
+	
+        CalendarEventProvider dataSource = new BasicEventProvider();
+        Calendar cal = new Calendar(dataSource);
+        cal.setWidth("260px");
+        cal.setHeight("500px");
+		cal.setFirstVisibleHourOfDay(6);
+		cal.setLastVisibleHourOfDay(20);
+		cal.setFirstVisibleDayOfWeek(1);
+		cal.setLastVisibleDayOfWeek(5);
+		
+		cal.setWeeklyCaptionFormat("dd-MM-yyyy");
+		
+
+		
+		return cal;
+	}
 }
