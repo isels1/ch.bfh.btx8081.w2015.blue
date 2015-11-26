769aebd07ccc126dcc9035bce1d230c0b5f432b8
isels1 <stefandaniel.iseli@gmail.com>
2015-11-26 08:40:36 +0100

    added time zone and a comment (IMPORTANT) ;)

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/CalendarView.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/CalendarView.java
@@ -2,8 +2,11 @@ package ch.bfh.btx8081.w2015.blue.HealthVisApp.View;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import com.vaadin.ui.Calendar;
+import com.vaadin.ui.Calendar.TimeFormat;
 import com.vaadin.ui.components.calendar.event.BasicEvent;
 import com.vaadin.ui.components.calendar.event.BasicEventProvider;
 import com.vaadin.ui.components.calendar.event.CalendarEventProvider;
@@ -18,12 +21,18 @@ public class CalendarView {
         Calendar cal = new Calendar(dataSource);
         cal.setWidth("260px");
         cal.setHeight("500px");
-      //show only 0600h to 2000h
+        //show only 0600h to 2000h
         cal.setFirstVisibleHourOfDay(6);
 		cal.setLastVisibleHourOfDay(20);
 		//show only Monday to Friday
-		cal.setFirstVisibleDayOfWeek(1);
-		cal.setLastVisibleDayOfWeek(5);
+		// --IMPORTANT: do only if weekly view. Daily and Monthly like that is buggy as fu**
+//		cal.setFirstVisibleDayOfWeek(1);
+//		cal.setLastVisibleDayOfWeek(5);
+		
+		//German Language and time zone 
+		cal.setLocale(Locale.GERMAN);
+		cal.setTimeFormat(TimeFormat.Format24H);
+		cal.setTimeZone(TimeZone.getTimeZone("Europe/Zurich"));
 		
 		//show daily view of calendar
 		cal.setStartDate(new Date());
@@ -37,7 +46,7 @@ public class CalendarView {
 		        "Learning how to use Vaadin Calendar",
 		        start.getTime(), end.getTime()));
 		
-		cal.setWeeklyCaptionFormat("dd-MM-yyyy");
+		cal.setWeeklyCaptionFormat("dd.MM.yyyy");
 		
 
 		
