8b54d8dbf0d631c36133d39d190d2f2dc3ef52f9
nosec1 <christinekatharina.noser@students.bfh.ch>
2015-11-26 14:48:28 +0100

    New Interface

---
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Interface/InterfaceCalendar.java
@@ -0,0 +1,10 @@
+package ch.bfh.btx8081.w2015.blue.HealthVisApp.Interface;
+
+//Interface for the Calendar Class
+public interface InterfaceCalendar {
+
+	public void newAppointment();
+	
+	public boolean checkAvailability();
+	
+}
