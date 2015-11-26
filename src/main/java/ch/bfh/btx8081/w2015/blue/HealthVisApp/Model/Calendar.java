ffcd831457bab84f038dd614b097884059717390
nosec1 <christinekatharina.noser@students.bfh.ch>
2015-11-26 16:19:40 +0100

    wrote checkAvailability and newAppointment.

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Model/Calendar.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Model/Calendar.java
@@ -1,5 +1,6 @@
 package ch.bfh.btx8081.w2015.blue.HealthVisApp.Model;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class Calendar {
@@ -14,4 +15,46 @@ public class Calendar {
 	}
 	
 
+	public void newAppointment(Date startTime, Date endTime, String ort, 
+									  Patient patient, HealthVisitor healthvisitor)
+	{
+		Appointment newAppointment = new Appointment(startTime, endTime, ort, patient, healthvisitor);
+		if(checkAvailability(newAppointment)) {
+		appointment.add(newAppointment);
+		}
+	}
+	
+	
+	private boolean checkAvailability(Appointment app)
+	{
+		for (Appointment i: appointment )
+		{
+			//Existing times to compare 
+			Date oldStartTime = i.getStartTime();
+			Date oldEndTime = i.getEndTime();
+			
+			//new time to compare
+			Date newStartTime = app.getStartTime();
+			Date newEndTime = app.getEndTime();
+			
+			if (!((newStartTime.before(oldStartTime) && 
+					(newEndTime.before(oldStartTime) || newEndTime.equals(oldStartTime)))
+				||((newStartTime.after(oldEndTime) || newStartTime.equals(oldEndTime))
+					&& (newEndTime.after(oldEndTime))))
+					)  
+			{
+				return false;
+			}
+			
+
+			
+			
+		}		
+		return true;
+		
+	}
+	
+	
+	
+	
 }
