0793e1450bc72d53a30820220d4ed6663e3d9bd0
zwahf1 <faebu.zwahlen@outlook.com>
2015-12-07 09:52:43 +0100

    adapt constructor

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/AppointmentPopUpView.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/AppointmentPopUpView.java
@@ -1,21 +1,12 @@
 package ch.bfh.btx8081.w2015.blue.HealthVisApp.View;
 
 public class AppointmentPopUpView {
-
-	private static AppointmentPopUpView appPopUpView = null;
 	
 	//================================================================================
     // Constructor Section
     //================================================================================
 	
-	private AppointmentPopUpView() {
+	public AppointmentPopUpView() {
 		
-	}
-	
-	public static AppointmentPopUpView getInstance() {
-		if (appPopUpView == null) {
-			appPopUpView = new AppointmentPopUpView();
-		}
-		return appPopUpView;
 	}
 }
--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/PatientListPopUpView.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/View/PatientListPopUpView.java
@@ -1,21 +1,12 @@
 package ch.bfh.btx8081.w2015.blue.HealthVisApp.View;
 
 public class PatientListPopUpView {
-
-	private static PatientListPopUpView patListPopUpView = null;
 	
 	//================================================================================
     // Constructor Section
     //================================================================================
 	
-	private PatientListPopUpView() {
+	public PatientListPopUpView() {
 		
-	}
-	
-	public static PatientListPopUpView getInstance() {
-		if (patListPopUpView == null) {
-			patListPopUpView = new PatientListPopUpView();
-		}
-		return patListPopUpView;
 	}
 }
