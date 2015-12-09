b3a793492df37bb8f732bc97e466f149ff657bb6
isels1 <stefandaniel.iseli@gmail.com>
2015-12-09 14:15:07 +0100

    für di timtimtim

---
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Controller/PatientController.java
@@ -0,0 +1,22 @@
+package ch.bfh.btx8081.w2015.blue.HealthVisApp.Controller;
+
+import java.util.ArrayList;
+
+import ch.bfh.btx8081.w2015.blue.HealthVisApp.Model.HealthVisitor;
+import ch.bfh.btx8081.w2015.blue.HealthVisApp.Model.Patient;
+import ch.bfh.btx8081.w2015.blue.HealthVisApp.Test.DummyDataCreator;
+
+public class PatientController {
+	
+	private DummyDataCreator ddc;
+	private HealthVisitor hv;
+	
+	public PatientController(){
+		ddc = DummyDataCreator.getInstance();
+		hv = ddc.getHealthVisitor();
+	}
+	
+	public ArrayList<Patient> retrievePatients(){
+		return hv.getPatient();
+	}
+}
