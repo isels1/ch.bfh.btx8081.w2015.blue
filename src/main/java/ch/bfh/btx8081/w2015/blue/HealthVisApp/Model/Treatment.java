f5f61cf6e30235dc5e06a95e906a7527634bc149
nosec1 <christinekatharina.noser@students.bfh.ch>
2015-12-16 13:47:06 +0100

    Added toString method

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Model/Treatment.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Model/Treatment.java
@@ -5,15 +5,46 @@ public class Treatment {
     // Treatment Data
     //================================================================================
 	
+	String treatment;
 	//================================================================================
-    // Constructior Section
+    // Constructor Section
     //================================================================================
 	
+	/**
+	 * This constructor is used for testing.
+	 */
+	
+	public Treatment() {
+	}
 	//================================================================================
     // Setter Section
     //================================================================================
+	/**
+	 * The getter for the treatment
+	 * @return treatment the Treatment /String
+	 */
 	
+	public String getComment() {
+		return treatment;
+	}
 	//================================================================================
     // Getter Section
     //================================================================================
+	/**
+	 * The getter for the comment
+	 * @return comment the Comment /String
+	 */
+	
+	public String getTreatment() {
+		return treatment;
+	}
+	
+	/**
+	 * The string representation is "treatment"
+	 * @return the string representation of the treatment /String
+	 */
+	@Override
+	public String toString() {
+		return treatment;
+	}
 }
