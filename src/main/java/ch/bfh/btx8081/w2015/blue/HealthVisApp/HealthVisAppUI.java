c682b34eb2f7dfc5fd71a5da5581e57e7b62815f
isels1 <stefandaniel.iseli@gmail.com>
2015-12-16 22:07:33 +0100

    added class javadoc part 3

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/HealthVisAppUI.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/HealthVisAppUI.java
@@ -13,7 +13,51 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- *
+ * Class
+ * <span class="courier">
+ * <font face="courier new">
+ * 		HealthVisAppUI
+ * </font>
+ * </span>
+ * <span class="courier">
+ * <font face="courier new">
+ * 		<b>extends</b> UI
+ * </font>
+ * </span>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		...main class of the health vis app project.
+ * </span>
+ * <br>
+ * <br>
+ * The HealthVisApp contains following information:
+ * 	<ul>
+ * 		<li>
+ * 			<font face="courier new">
+ * 				final WITDH:
+ * 			</font> 
+ * 			<i>String</i>
+ * 			- The width of the gui
+ * 		</li>
+ * 		<li>
+ * 			<font face="courier new">
+ * 				final HEIGHT:
+ * 			</font> 
+ * 			<i>String</i>
+ * 			- The height of the gui
+ * 		</li>
+ *	 </ul>
+ * <br>
+ * <b>
+ * 		Creation date:
+ * </b>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		xx.xx.2015
+ * </span>
+ * <br><br>
+ * @author zwahf1, dornt1, isels1
+ * @version 1.0
  */
 @SuppressWarnings("serial")
 @Theme("HealthVisAppTheme")
