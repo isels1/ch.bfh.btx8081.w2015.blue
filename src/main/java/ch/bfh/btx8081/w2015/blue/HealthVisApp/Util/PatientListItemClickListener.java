a01ca2e9a22deb7b03005604ff66e393ce4758e8
isels1 <stefandaniel.iseli@gmail.com>
2015-12-17 08:48:21 +0100

    Class JavaDoc part 2

--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Util/PatientListItemClickListener.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Util/PatientListItemClickListener.java
@@ -4,6 +4,36 @@ import ch.bfh.btx8081.w2015.blue.HealthVisApp.Controller.UIController;
 
 import com.vaadin.event.ItemClickEvent;
 
+/**
+ * Class
+ * <span class="courier">
+ * <font face="courier new">
+ * 		PatientListItemClickListener
+ * </font>
+ * </span>
+ * <span class="courier">
+ * <font face="courier new">
+ * 		<b>implements</b> ItemClickEvent.ItemClickListener
+ * </font>
+ * </span>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		...control click events from the patient list to open edit patient view
+ * </span>
+ * <br>
+ * <br>
+ * <b>
+ * 		Creation date:
+ * </b>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		09.12.2015
+ * </span>
+ * <br><br>
+ * @author isels1
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
 public class PatientListItemClickListener implements ItemClickEvent.ItemClickListener {
 
 	@Override
--- src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Util/PatinetListComboBoxChangeListener.java
+++ src/main/java/ch/bfh/btx8081/w2015/blue/HealthVisApp/Util/PatinetListComboBoxChangeListener.java
@@ -1,13 +1,50 @@
 package ch.bfh.btx8081.w2015.blue.HealthVisApp.Util;
 
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label.ValueChangeEvent;
 
 import ch.bfh.btx8081.w2015.blue.HealthVisApp.Controller.PatientListController;
-import ch.bfh.btx8081.w2015.blue.HealthVisApp.Controller.UIController;
-import ch.bfh.btx8081.w2015.blue.HealthVisApp.View.PatientListView;
 
+/**
+ * Class
+ * <span class="courier">
+ * <font face="courier new">
+ * 		PatinetListComboBoxChangeListener
+ * </font>
+ * </span>
+ * <span class="courier">
+ * <font face="courier new">
+ * 		<b>implements</b> ComboBox.ValueChangeListener
+ * </font>
+ * </span>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		...control the combobox chanche events from the patient list filter combobox to filter the patient view.
+ * </span>
+ * <br>
+ * <br>
+ * The PatinetListComboBoxChangeListener contains following information:
+ * 	<ul>
+ * 		<li>
+ * 			<font face="courier new">
+ * 				patListController:
+ * 			</font> 
+ * 			<i>PatientListController</i>
+ * 			- PatientListController object for the filter options
+ * 		</li>
+ *	 </ul>
+ * <br>
+ * <b>
+ * 		Creation date:
+ * </b>
+ * <br>
+ * <span style="margin-left:60px;">
+ * 		xx.xx.2015
+ * </span>
+ * <br><br>
+ * @author zwahf1, dornt1, isels1
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
 public class PatinetListComboBoxChangeListener implements ComboBox.ValueChangeListener{
 
 	//================================================================================
