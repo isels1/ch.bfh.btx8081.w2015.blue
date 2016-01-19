03c31bbf9d180566bd975ed6d456e93fd508a540
isels1 <stefandaniel.iseli@gmail.com>
2016-01-19 16:04:38 +0100

    directories for tests!!

---
+++ src/test/java/btx8081_w2015_blue/DUMMY.java
@@ -0,0 +1,14 @@
+package btx8081_w2015_blue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DUMMY {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
